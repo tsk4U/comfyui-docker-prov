17ab439688f51b2d925f388a52309db4e5120dbb
tsk4U <163897398+tsk4U@users.noreply.github.com>
2024-07-29 22:20:05 +0200

    Update default.sh

--- default.sh
+++ default.sh
@@ -13,18 +13,22 @@ PYTHON_PACKAGES=(
 NODES=(
     "https://github.com/ltdrdata/ComfyUI-Manager"
     "https://github.com/ltdrdata/ComfyUI-Impact-Pack"
-    "https://github.com/ltdrdata/ComfyUI-Inspire-Pack"
+    #"https://github.com/ltdrdata/ComfyUI-Inspire-Pack"
+    "https://github.com/WASasquatch/was-node-suite-comfyui"
     "https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
+    "https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes"
+    "https://github.com/huchenlei/ComfyUI-layerdiffuse"
     "https://github.com/Gourieff/comfyui-reactor-node"
     "https://github.com/rgthree/rgthree-comfy"
-    "https://github.com/giriss/comfy-image-saver"
-    "https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite"
-    "https://github.com/storyicon/comfyui_segment_anything"
+    #"https://github.com/giriss/comfy-image-saver"
+    #"https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite"
+    #"https://github.com/storyicon/comfyui_segment_anything"
 )
 
 CHECKPOINT_MODELS=(
     #"https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
     "https://civitai.com/api/download/models/372799?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=c8a0a37d25645dfe92bf8e2c174f1806"
+    "https://civitai.com/api/download/models/456194?type=Model&format=SafeTensor&size=full&fp=fp16&token=c8a0a37d25645dfe92bf8e2c174f1806"
     #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
@@ -32,6 +36,7 @@ CHECKPOINT_MODELS=(
 
 LORA_MODELS=(
     #"https://civitai.com/api/download/models/16576"
+    "https://civitai.com/api/download/models/412357?type=Model&format=SafeTensor&token=c8a0a37d25645dfe92bf8e2c174f1806"
 )
 
 VAE_MODELS=(
